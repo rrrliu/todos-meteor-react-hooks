41348868bcad0b3f82b9ca9a8d11f8034aec9463
Richard Liu <richard.y.liu@berkeley.edu>
2020-06-24 03:24:01 -0700

    finished tests

--- tests/main.js
+++ tests/main.js
@@ -1,19 +1,20 @@
-import assert from "assert";
+import assert from 'assert';
+import '../imports/api/tasks.tests';
 
-describe("meteor-todos", function () {
-  it("package.json has correct name", async function () {
-    const { name } = await import("../package.json");
-    assert.strictEqual(name, "meteor-todos");
+describe('meteor-todos', function () {
+  it('package.json has correct name', async function () {
+    const { name } = await import('../package.json');
+    assert.strictEqual(name, 'meteor-todos');
   });
 
   if (Meteor.isClient) {
-    it("client is not server", function () {
+    it('client is not server', function () {
       assert.strictEqual(Meteor.isServer, false);
     });
   }
 
   if (Meteor.isServer) {
-    it("server is not client", function () {
+    it('server is not client', function () {
       assert.strictEqual(Meteor.isClient, false);
     });
   }
